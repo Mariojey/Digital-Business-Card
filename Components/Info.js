37ef40aba52bc3cac215279f80281cb51a5886af
Mariusz Jacek <marios.yaceg@gmail.com>
2022-07-14 11:26:05 +0200

    Fix or Dix

---
+++ Components/Info.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Info(){
+    return(
+        <div className="header">
+            <img src="./public/MegaFria.png" alt=""/>
+            <h1 className="mariojey-header-text">Mariusz Jacek</h1>
+            <p className="full-stack-dev-header-text">Full-Stack Developer</p>
+            <div className="header-icons">
+                <a href="mariuszjacek3bip@gmail.com" className="link-gmail">
+                    <img src="./public/gmail-logo-24.png" alt="" />
+                    <p>Email</p>
+                </a>
+                <a href="https://www.linkedin.com/in/mariusz-jacek-9b02b81b7/?originalSubdomain=pl" className="link-linkedin">
+                    <img src="./public/linkedin-logo-24.png" alt="" />
+                    <p>LinkedIn</p>
+                </a>
+            </div>
+        </div>
+    )
+}
---
+++ Components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests(){
+    return(
+        <div className="interests-section">
+            <h1 className="text-header">Interests</h1>
+            <p className="paragraph-text">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
